b7528f9813249e01bf67d4d16d34731e309be8bd
Icaro123 <icaro.tuts@gmail.com>
2014-06-02 15:11:10 -0300

    Exceptions adicionadas

---
+++ src/br/uefs/ecomp/visWarm/model/exception/VideoJaExisteException.java
@@ -0,0 +1,18 @@
+package br.uefs.ecomp.VisWarm.model.exception;
+
+public class VideoJaExisteException extends Exception {
+
+    public VideoJaExisteException() {
+        super();
+    }
+
+    public VideoJaExisteException(String msg) {
+        super(msg);
+    }
+
+    public VideoJaExisteException(Throwable t) {
+        super(t);
+
+    }
+
+}
---
+++ src/br/uefs/ecomp/visWarm/model/exception/VideoNaoEncontradoException.java
@@ -0,0 +1,19 @@
+package br.uefs.ecomp.VisWarm.model.exception;
+
+public class VideoNaoEncontradoException extends Exception {
+
+    public VideoNaoEncontradoException() {
+        super();
+    }
+
+    public VideoNaoEncontradoException(String msg) {
+        super(msg);
+    }
+
+    public VideoNaoEncontradoException(Throwable t) {
+        super(t);
+
+    }
+
+}
+
